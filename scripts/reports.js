9096fe6c926f9e3d7b4109f898bfb89efd2631ea
Pascal Duez <public@pascalduez.me>
2017-10-23 00:40:49 +0200

    Remove dupe file

--- scripts/reports.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-// process.env.JEST_SUITE_NAME = 'react-module-boilerplate unit tests';
-// process.env.JEST_JUNIT_OUTPUT = './reports/junit-report.xml';
-
-module.exports = function (result) {
-  require('jest-junit')(result);
-  require('jest-html-reporter')(result);
-}
